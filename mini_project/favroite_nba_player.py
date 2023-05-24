ce4712936d17198591fb7a367a332de3c6bed293
kambojason <kambojason15@gmail.com>
2023-05-24 23:43:18 +0000

    Mini Project Lab 35

---
+++ mini_project/favroite_nba_player.py
@@ -0,0 +1,13 @@
+#!usr/bin/env python3
+
+# Will determine your favroite NBA player from My top 10 list
+
+top_10_players = ["LeBron James", "Michael Jordon", "Shaquille O'Neal", "Kareem Abdul-Jabbar", "Kobe Bryant", "Steph Curry", "Wilt Chamberlain", "Bill Russell", "Magic Johnson", "Dirk Nowitzki"]
+
+print(f"I will determine your favroite NBA player from my top 10 list, in no particular order. \nThe players you have to choose from are:")
+
+for player in top_10_players:
+    print(player)
+
+
+
