000cb41469d2bc303f029105d8187b7e25dd1392
kambojason <kambojason15@gmail.com>
2023-05-26 15:44:44 +0000

    Playing with python pip package

---
+++ color/color3.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+import crayons
+
+def main():
+
+    print(crayons.green('I like using green'))
+    
+    # can only use bold true statetment like below have to dissable crayons first
+
+    #print(crayons.cyan("this cyan clor is cool", bold=True))
+
+
+#if __name__="__name__":
+main()
