29fd27c44c0e10f8085a76a03669b0bc8b8c5037
kambojason <kambojason15@gmail.com>
2023-06-05 01:18:46 +0000

    Final almost done

--- challenge67/final_project_maybe.py
+++ challenge67/final_project_maybe.py
@@ -1,6 +1,7 @@
 #!usr/bin/env python3
 
 import csv
+import time
 
 # Get the list of unique devil fruit classes from the data
 devil_fruit_classes = set()
@@ -20,8 +21,9 @@ for fruit_class in devil_fruit_classes:
 while True:
     # Read the devil fruit class from the user
     devil_fruit_class = input("Enter the devil fruit class you want to know about: ").lower()
-
     if devil_fruit_class in devil_fruit_classes:
+        print("Loading...")
+        time.sleep(3)  # Sleep for 3 seconds to simulate loading
         break
     else:
         print("Invalid class. Please try again.")
@@ -43,4 +45,22 @@ print(f"Number of devil fruits with {devil_fruit_class} class type: {logia_count
 print(f"The characters with the {devil_fruit_class} type devil fruit are:")
 for character_in_show in logia_characters:
     print(character_in_show)
+
+while True:
+    chosen_character = input("Which of these characters do you want to see? ")
+    if chosen_character.lower() in [character.lower() for character in logia_characters]:
+        print("Loading...")
+        time.sleep(3)  # Sleep for 3 seconds to simulate loading
+        break
+    else:
+        print("Invalid character. Please try again.")
+
+if chosen_character.lower() == "monkey d. luffy":
+    with open("luffy_pic.txt", "r") as file:
+        luffy_pic_data = file.read()
+        print(luffy_pic_data)
+else:
+    print("Sorry, no picture file loaded yet for this character.")
+
+input("Press Enter to exit...")
 
